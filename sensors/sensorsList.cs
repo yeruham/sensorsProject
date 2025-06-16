c9452077870838354ba6d1120abaafd487adf6a1
YeruhamMendelson <y0583207549@gmail.com>
2025-06-16 09:57:32 +0300

    class sensorList - list wite sensors

--- sensors/sensorsList.cs
+++ sensors/sensorsList.cs
@@ -1,1 +1,26 @@
-﻿
+﻿using System.Collections.Generic;
+
+public class SensorList
+{
+    private List<Sensor> sensors;
+    
+    public SensorList()
+    {
+        sensors = new List<Sensor>();
+    }
+
+    public void addSensor(Sensor sensor)
+    {
+        sensors.Add(sensor);
+    }
+
+    public void removeSensor(Sensor sensor)
+    {
+        sensors.Remove(sensor);
+    }
+
+    public List<Sensor> getSensors()
+    {
+        return this.sensors;
+    }
+}
