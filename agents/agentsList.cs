8fab490c92d2eb2ce2d7d159287a48038fdf9b5f
YeruhamMendelson <y0583207549@gmail.com>
2025-06-16 11:05:08 +0300

    write class of agentsList

--- agents/agentsList.cs
+++ agents/agentsList.cs
@@ -1,1 +1,22 @@
-﻿
+﻿using System.Collections.Generic;
+
+public class AgentsList
+{
+    public List<Agent> agents;
+
+    public AgentsList()
+    {
+        agents = new List<Agent>();
+    }
+
+    public void addSensor(Agent agent)
+    {
+        agents.Add(agent);
+    }
+
+    public void removeSensor(Agent agent)
+    {
+        agents.Remove(agent);
+    }
+
+}
