89de2b3fc2d4bdcf546161b10a8f8f4c724504a3
YeruhamMendelson <y0583207549@gmail.com>
2025-06-16 10:14:36 +0300

    write class of investigation

--- investigations/investigation.cs
+++ investigations/investigation.cs
@@ -1,1 +1,43 @@
-﻿
+﻿using System.Collections.Generic;
+
+public class Investigation
+{
+    private Sensor[] attachedSensors;
+    public Agent agent { get; }
+
+    public Investigation(Agent agent)
+    {
+        this.agent = agent;
+        this.attachedSensors = new Sensor[agent.numSensors];
+    }
+
+    public void addSensor(Sensor sensor)
+    {
+        for(int i = 0; i < this.attachedSensors.Length; i++)
+        {
+            if (this.attachedSensors[i] == null)
+            {
+                this.attachedSensors[i] = sensor;
+                break;
+            }
+        }
+    }
+
+    public float activateSensors()
+    {
+        bool succeeded = false;
+        float compatibleSensors = 0;
+
+        for (int i = 0; i < this.attachedSensors.Length; i++)
+        {
+            succeeded = this.attachedSensors[i].activate(this.agent);
+            if (!succeeded)
+            {
+                this.attachedSensors[i] = null;
+            }
+        }
+
+        compatibleSensors = this.agent.compatibleSensors();
+        return compatibleSensors;
+    }
+}
