3d8ae82fb539c0fba8e6733fcca77a1d3767480d
YeruhamMendelson <y0583207549@gmail.com>
2025-06-16 09:51:16 +0300

    class of sensor (fields and method activate)

--- sensors/Sensor.cs
+++ sensors/Sensor.cs
@@ -1,1 +1,19 @@
-﻿
+﻿public class Sensor
+{
+    public string name { get; }
+    public string type { get; }
+
+    public Sensor(string name, string type)
+    {
+        this.name = name;
+        this.type = type;
+    }
+
+    public bool activate(Agent agent)
+    {
+        bool succeeded = false;
+        return succeeded;
+    }
+
+
+}
