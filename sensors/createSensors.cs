d183c491b35f794b3ad76e252fca01192511d01c
YeruhamMendelson <y0583207549@gmail.com>
2025-06-17 16:10:34 +0300

    new in createSensor to creat randomaly

--- sensors/createSensors.cs
+++ sensors/createSensors.cs
@@ -3,19 +3,29 @@
 public static class createSensors
 {
     private static Random _random = new Random();
+    private static string[] _typeSensors = { "thermal", "audio" };
+    private static string[] _names = { "A23", "V5", "F23", "P12", "G3", "N7", "C45", "W34", "Q12" };
     public static Sensor createSensor()
     {
-        Sensor sensor = null;
-        int num = _random.Next(1, 200);
-        string name = Convert.ToString(num);
-        if (num % 2 == 0)
-        {
-            sensor = new Sensor(name, "thermal");
-        }
-        else {
-            sensor = new Sensor(name, "audio");
-        }
+        string name = createName();
+        string type = createType();
+        Sensor sensor = new Sensor(name, type);
         return sensor;
+    }
+
+    private static string createName()
+    {
+        int num = _random.Next(0, _names.Length);
+        string name = _names[num];
+        _names[num] = Convert.ToString(_random.Next(100, 500));
+        return name;
+    }
+
+    private static string createType()
+    {
+        int num = _random.Next(0, _typeSensors.Length);
+        string type = _typeSensors[num];
+        return type;
     }
 
 }
