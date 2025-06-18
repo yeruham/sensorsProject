1e7067a615f1cc69471b4f63359752fadba5dcb3
YeruhamMendelson <y0583207549@gmail.com>
2025-06-18 09:33:07 +0300

    new sensor - signal

---
+++ sensors/signalSensor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class SignalSensor: Sensor
+{
+    public SignalSensor(string name) : base(name)
+    {
+        this.type = "Signal";
+    }
+
+    public override bool activate(Agent agent)
+    {
+        Console.WriteLine($"The rank of agent {agent.name} is {agent.rank}.");
+        return base.activate(agent);
+    }
+}
